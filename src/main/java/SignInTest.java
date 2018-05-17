2fe13e9144385a4eca92532a4e82422ef12c2bf2
bob <jrohit54@gmail.com>
2018-05-17 08:26:14 +0530

    add the code to handle frame

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -19,9 +19,10 @@ public class SignInTest {
 
         driver.findElement(By.linkText("Your trips")).click();
         driver.findElement(By.id("SignIn")).click();
-
+        driver.manage().window().maximize();
+        waitFor(2000);
+        driver.switchTo().frame("modal_window");
         driver.findElement(By.id("signInButton")).click();
-
         String errors1 = driver.findElement(By.id("errors1")).getText();
         Assert.assertTrue(errors1.contains("There were errors in your submission"));
         driver.quit();
